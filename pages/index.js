2ef956780f52316528f33d6c8e7d389a21f18bba
10BlackHole <borjadiez1014@gmail.com>
2022-06-08 04:07:29 -0400

    more

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Button, Container, Box, Heading, Image, Link, useColorModeValue } from '@chakra-ui/react'
+import { ChevronRightIcon } from  '@chakra-ui/icons'
 import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioSection , BioYear } from '../components/bio'
 
 const Page = () => {
 	return (
@@ -38,7 +42,47 @@ const Page = () => {
 				<Heading as="h3" variant="section-title">
 					Work
 				</Heading>
-				<p>Paragraph</p>
+				<Paragraph>
+					Borja is a physics student at the University of Concepcion, Chile. His hobbies include taking notes on physics. His latest projects can be found on {' '} 
+					<NextLink href="works/github">
+						<Link>Github</Link>
+					</NextLink>
+					.
+				</Paragraph>
+				<Box align="center" my={4}>
+					<NextLink href="/works">
+					<Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+						My portfolio
+					</Button>
+					</NextLink>
+					</Box>
+			</Section>
+
+			<Section delay={0.2}>
+				<Heading as="h3" variant="section-title">
+					Bio
+				</Heading>
+				<BioSection>
+					<BioYear>2001</BioYear>
+					Born in Valdivia, Chile.
+				</BioSection>
+				<BioSection>
+					<BioYear>2019 to present</BioYear>
+					Go to university to study physics.
+				</BioSection>
+			</Section>
+
+			<Section>
+				<Heading as="h3" variant="section-title">
+					I love
+				</Heading>
+				<Paragraph>
+					Physics,{' '}
+					<Link href="https://open.spotify.com/playlist/1ZpvW0jQkqGu1Aekakkb9r?si=60f2d27317a74a01">
+						Music
+					</Link>
+					, Write.
+				</Paragraph>
 			</Section>
 		</Container>
 	)
