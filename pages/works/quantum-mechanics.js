1acdba25dfcbd97421966e84bec9d13229f8a3b0
10BlackHole <borjadiez1014@gmail.com>
2022-06-09 03:14:05 -0400

    caracteres

--- pages/works/quantum-mechanics.js
+++ pages/works/quantum-mechanics.js
@@ -27,7 +27,7 @@ const Work = () => {
 				Preface	
 				</Heading>
 				<P>
-				Unlike Newton's	 mechanics, or Maxwell's electrodynamics, or Einstein's relativity, quantum theory was not created (or even definitively packaged) by one individual, and it retains to this day some of the scars of its exhilaranting byt traumatic youth. There is no general consensus as to  what its fundamental principles are, how it should be taught, or what it really "means". Every competent physicist can "do" quantum mechanics, but the stories we tell ourselves about what we are doing are as various as the tales of Scheharazade, and almost as implausible. Niels Bohr said, "If you are not confused by quantum physics the you haven't really understood it"; Richard Feynman remarked, "I think I can safely say that nobody understands quantum mechanics".
+				Unlike Newton&apos;s	 mechanics, or Maxwell&apos;s electrodynamics, or Einstein&apos;s relativity, quantum theory was not created (or even definitively packaged) by one individual, and it retains to this day some of the scars of its exhilaranting byt traumatic youth. There is no general consensus as to  what its fundamental principles are, how it should be taught, or what it really &#34;means&#34;. Every competent physicist can &#34;do&#34; quantum mechanics, but the stories we tell ourselves about what we are doing are as various as the tales of Scheharazade, and almost as implausible. Niels Bohr said, &#34;If you are not confused by quantum physics the you haven&apos;t really understood it&#34;; Richard Feynman remarked, &#34;I think I can safely say that nobody understands quantum mechanics&#34;.
 				</P>
 				
 				<Section>
